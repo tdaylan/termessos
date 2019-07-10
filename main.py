06e0e442f6167133084fee483c9d1e7094833541
Tansu Daylan <tansu.daylan@gmail.com>
2019-07-10 11:24:06 -0400

    N/A

--- main.py
+++ main.py
@@ -13,19 +13,11 @@ import matplotlib.pyplot as plt
 import h5py 
 import os, sys
 import emcee
+import tdpy.mcmc
 
 import dynesty
 from dynesty import plotting as dyplot
 from dynesty import utils as dyutils
-
-def summgene(arry):
-    try: 
-        print np.amin(arry)
-        print np.amax(arry)
-        print np.mean(arry)
-        print arry.shape
-    except:
-        print arry
 
 
 def icdf(para, numbepoc):
@@ -97,6 +89,9 @@ time = np.copy(indxepocobsv)
 # 'emce' or 'nest'
 samptype = sys.argv[1]
 
+pathmetd = pathdata + samptype + '/'
+os.system('mkdir -p %s' % pathmetd)
+
 listttvrtype = ['cons', 'sinu']
 for ttvrtype in listttvrtype:
     if ttvrtype == 'cons':
@@ -105,8 +100,8 @@ for ttvrtype in listttvrtype:
         numbpara = 4
     
     if samptype == 'emce':
-        numbsamp = 100000
-        numbsampburn = 50000
+        numbsamp = 200000
+        numbsampburn = 20000
     
     numbdoff = numbepoc - numbpara
     
@@ -115,17 +110,27 @@ for ttvrtype in listttvrtype:
     # offs
     limtpara[0, 0] = -0.2
     limtpara[1, 0] = 0.2
+    listlablpara = ['$C$ [minutes]']
     if ttvrtype == 'sinu':
         # phas
-        limtpara[0, 1] = 0.8 * np.pi
-        limtpara[1, 1] = 1.5 * np.pi
+        limtpara[0, 1] = 0.#np.pi
+        limtpara[1, 1] = 2. * np.pi
         # ampl
         limtpara[0, 2] = 0.
-        limtpara[1, 2] = 1. * np.pi
+        limtpara[1, 2] = 1.
         # peri
-        limtpara[0, 3] = 14.
-        limtpara[1, 3] = 22.
+        limtpara[0, 3] = 2.
+        limtpara[1, 3] = 15.
     
+        listlablpara += ['$\phi$', '$A$ [minutes]', '$P$ [epochs]']
+    
+    numbbins = 60
+    indxbins = np.arange(numbbins)
+    binspara = np.empty((numbbins + 1, numbpara)) 
+    for k in indxpara:
+        binspara[:, k] = np.linspace(limtpara[0, k], limtpara[1, k], numbbins + 1)
+    meanpara = (binspara[1:, :] + binspara[:-1, :]) / 2.
+
     dictllik = [time, numbepoc, ttvrobsv, ttvrstdvobsv, ttvrtype]
     dicticdf = [numbepoc]
     
@@ -169,6 +174,14 @@ for ttvrtype in listttvrtype:
         samppara = dyutils.resample_equal(results.samples, weights)
         assert samppara.size == results.samples.size
     
+    if samptype == 'emce':
+        listsamp = objtsave.flatchain
+        listllik = objtsave.flatlnprobability
+    else:
+        listsamp = samppara[:, k]
+   
+    indxsampmlik = np.argmax(listllik)
+
     # plot the posterior
     ## parameter
     ### trace
@@ -177,30 +190,35 @@ for ttvrtype in listttvrtype:
             figr, axis = plt.subplots()
             for i in indxwalk:
                 axis.plot(indxsampwalk[::10], objtsave.chain[i, ::10, k])
-            path = pathdata + 'tracwalk%04d_%s.pdf' % (k, ttvrtype)
+            path = pathdata + '%s/tracwalk%04d_%s.pdf' % (samptype, k, ttvrtype)
             print 'Writing to %s...' % path
             plt.savefig(path)
             plt.close()
         
     ### histogram
     for k in indxpara:
-        figr, axis = plt.subplots()
-        if samptype == 'emce':
-            axis.hist(objtsave.flatchain[:, k]) 
-        else:
-            axis.hist(samppara[:, k]) 
-        path = pathdata + 'hist%04d_%s.pdf' % (k, ttvrtype)
+        figr, axis = plt.subplots(figsize=(6, 4))
+        axis.hist(listsamp[:, k], numbbins) 
+        axis.set_ylabel('$N_{samp}$')
+        axis.set_xlabel(listlablpara[k])
+        path = pathdata + '%s/hist%04d_%s.pdf' % (samptype, k, ttvrtype)
+        plt.tight_layout()
         print 'Writing to %s...' % path
         plt.savefig(path)
         plt.close()
     
+    path = pathdata + '%s/' % samptype
+    strgplot = 'post_%s' % ttvrtype
+
+    listparamlik = listsamp[indxsampmlik, :]
+    tdpy.mcmc.plot_grid(path, strgplot, listsamp, listlablpara, listvarbdraw=[listparamlik], numbbinsplot=numbbins)
     
     if samptype == 'nest':
         for keys in objtsave:
             if isinstance(objtsave[keys], np.ndarray) and objtsave[keys].size == numbsamp:
                 figr, axis = plt.subplots()
                 axis.plot(indxsamp, objtsave[keys])
-                path = pathdata + '%s_%s.pdf' % (keys, ttvrtype)
+                path = pathdata + '%s/%s_%s.pdf' % (samptype, keys, ttvrtype)
                 print 'Writing to %s...' % path
                 plt.savefig(path)
     else:
@@ -211,16 +229,25 @@ for ttvrtype in listttvrtype:
                 axis.plot(indxsampwalk[::10], objtsave.lnprobability[::10, i])
         else:
             axis.plot(indxsamp, objtsave['logl'])
-        path = pathdata + 'llik_%s.pdf' % ttvrtype
+        path = pathdata + '%s/llik_%s.pdf' % (samptype, ttvrtype)
         print 'Writing to %s...' % path
         plt.savefig(path)
         plt.close()
     
         chi2 = -2. * objtsave.lnprobability
+        
         print 'Posterior-mean chi2: '
         print np.mean(chi2)
         print 'Posterior-mean chi2 per dof: '
         print np.mean(chi2) / numbdoff
+        print 'Minimum chi2: '
+        print np.amin(chi2)
+        print 'Minimum chi2 per dof: '
+        print np.amin(chi2) / numbdoff
+        print 'Posterior-mean llik: '
+        print np.mean(objtsave.lnprobability)
+        print 'Maximum llik: '
+        print np.amax(objtsave.lnprobability)
     
     
     ### sample model ttvr
@@ -231,7 +258,7 @@ for ttvrtype in listttvrtype:
     yerr[0, :] = ttvrstdvobsv
     yerr[1, :] = ttvrstdvobsv
     
-    numbepocfine = 100
+    numbepocfine = 1000
     indxepocfine = np.linspace(np.amin(indxepocobsv), np.amax(indxepocobsv), numbepocfine)
     ttvrmodlfine = np.empty((numbsamp, numbepocfine))
     for k in indxttvrmodl:
@@ -248,6 +275,27 @@ for ttvrtype in listttvrtype:
             peri = objttemp[k, 3]
             ttvrmodlfine[k, :] = retr_ttvrmodl(indxepocfine, offs, phas, ampl, peri)
     
+    if ttvrtype == 'cons':
+        mlikcons = np.amax(listllik)
+
+    if ttvrtype == 'sinu':
+        llikperi = np.empty(numbbins)
+        for m in indxbins:
+            indxsampthis = np.where((listsamp[:, -1] < binspara[m+1, -1]) & (listsamp[:, -1] > binspara[m, -1]))
+            llikperi[m] = np.amax(listllik[indxsampthis])
+        
+        figr, axis = plt.subplots(figsize=(6, 3))
+        axis.plot(meanpara[:, -1], -2. * llikperi, label='Sinusoidal')
+        axis.axhline(-2. * mlikcons, label='Constant', color='orange')
+        axis.set_xlabel('Period [epochs]')
+        axis.set_ylabel('$\chi^2$')
+        plt.tight_layout()
+        plt.legend()
+        path = pathdata + '%s/chi2peri_%s.pdf' % (samptype, ttvrtype)
+        print 'Writing to %s...' % path
+        plt.savefig(path)
+        plt.close()
+    
     figr, axis = plt.subplots(figsize=(6, 3))
     axis.errorbar(indxepocobsv, ttvrobsv, yerr=yerr, color='black', marker='o', ls='')
     for k in indxttvrmodl:
@@ -255,7 +303,7 @@ for ttvrtype in listttvrtype:
     axis.set_xlabel('Epoch')
     axis.set_ylabel('Transit timing residuals [minute]')
     plt.tight_layout()
-    path = pathdata + 'modl_%s.pdf' % ttvrtype
+    path = pathdata + '%s/modl_%s.pdf' % (samptype, ttvrtype)
     print 'Writing to %s...' % path
     plt.savefig(path)
     plt.close()
@@ -263,19 +311,19 @@ for ttvrtype in listttvrtype:
     ### nested sampling specific
     if samptype == 'nest':
         rfig, raxes = dyplot.runplot(results)
-        path = pathdata + 'dyne_runs_%s.pdf' % ttvrtype
+        path = pathdata + '%s/dyne_runs_%s.pdf' % (samptype, ttvrtype)
         print 'Writing to %s...' % path
         plt.savefig(path)
         plt.close()
         
         tfig, taxes = dyplot.traceplot(results)
-        path = pathdata + 'dyne_trac_%s.pdf' % ttvrtype
+        path = pathdata + '%s/dyne_trac_%s.pdf' % (samptype, ttvrtype)
         print 'Writing to %s...' % path
         plt.savefig(path)
         plt.close()
         
         cfig, caxes = dyplot.cornerplot(results)
-        path = pathdata + 'dyne_corn_%s.pdf' % ttvrtype
+        path = pathdata + '%s/dyne_corn_%s.pdf' % (samptype, ttvrtype)
         print 'Writing to %s...' % path
         plt.savefig(path)
         plt.close()
---
+++ util.py
@@ -0,0 +1,249 @@
+import os, numpy as np
+import tdpy
+from tdpy.util import summgene
+import emcee
+import matplotlib.pyplot as plt
+import allesfitter
+
+
+def ttvr(pathdata, pathtmpt, epoc, peri, offs, liststrginst, booltmptmcmc=True, liststrgplan=['b']):
+    
+    print 'Starting the pipeline to infer per-transit TTVs...'
+    
+    # read ns_table of the global fit, constructing a template params.csv 
+    if booltmptmcmc:
+        strgtemp = 'mcmc'
+    else:
+        strgtemp = 'ns'
+    objtfile = open(pathtmpt + 'results/%s_table.csv' % strgtemp)
+    objtfilesave = open(pathtmpt + 'params.csv', 'w')
+    objtfilesave.write('#name,value,fit,bounds,label,unit\n')
+    for k, line in enumerate(objtfile):
+        if line.startswith('#'):
+            objtfilesave.write(line)
+        elif not ('CTIO' in line or \
+                'PEST' in line or \
+                'LCO-SSO-1m0_ip_2' in line or \
+                'TESS5' in line or \
+                'LCO-SAAO-1m_gp' in line or \
+                'LCO-SSO-1m0_gp' in line):
+            linesplt = line.split(',')
+            if 'TESS34spoc' in linesplt[0]:
+                linespltprim = linesplt[0].split('TESS34spoc')
+                linesplt[0] = linespltprim[0] + 'TESS' + linespltprim[1]
+            lineneww = linesplt[0] + ',' + linesplt[1] + ',0,uniform -1e12 1e12,temp,\n'
+            objtfilesave.write(lineneww)
+    objtfilesave.close()
+    
+    numbplan = len(liststrgplan)
+    indxplan = np.arange(numbplan)
+    
+    numbtran = np.empty(numbplan, dtype=int)
+    indxtran = []
+    numbtran = [10000 for k in range(numbplan)]
+    timetran = []
+    for k, strgplan in enumerate(liststrgplan):
+        indxtran.append(np.arange(numbtran[k]))
+        timetran.append(epoc[k] + (indxtran[k] - offs[k]) * peri[k])
+    
+    timetole = 0.25 # [days]
+    
+    pathpost = pathdata + 'allesfit_sttv/post/'
+    os.system('mkdir -p %s' % pathpost)
+    
+    print 'Maximum time away from the mid-transit that will be used in the fit:'
+    print timetole
+    numbinst = len(liststrginst)
+    arry = {}
+    time = {}
+    minmtime = 1e100
+    maxmtime = -1e100
+    for strginst in liststrginst:
+        temp = np.loadtxt(pathtmpt + strginst + '.csv', delimiter=',')
+        if strginst == 'TESS':
+            arry[strginst] = temp
+        else:
+            arry[strginst] = temp[:, :3]
+        time[strginst] = temp[:, 0]
+        minmtime = min(np.amin(time[strginst]), minmtime)
+        maxmtime = max(np.amax(time[strginst]), maxmtime)
+    limttime = [minmtime, maxmtime]
+    numbplan = len(liststrgplan)
+    indxplan = np.arange(numbplan)
+    indxtranskip = [[] for k in indxplan]
+    for k, strgplan in enumerate(liststrgplan):
+        for l in indxtran[k]:
+            pathtran = pathdata + 'allesfit_sttv/allesfit_%s%03d/' % (strgplan, l)
+            indx = {}
+            booltraninst = np.zeros(numbinst, dtype=bool)
+            for m, strginst in enumerate(liststrginst):
+                indx[strginst] = np.where(abs(time[strginst] - timetran[k][l]) < timetole)[0]
+                print 'klm'
+                print k,l,m
+                print 'strginst'
+                print strginst
+                print 'timetran[k][l]'
+                print timetran[k][l]
+                print 'time[strginst]'
+                summgene(time[strginst])
+                print
+                if indx[strginst].size > 0:
+                    booltraninst[m] = True
+                
+            if np.where(booltraninst)[0].size == 0:
+                indxtranskip[k].append(l)
+                continue
+    
+            print 'Planet %s, transit %d' % (strgplan, l)
+            print 'Transit time:'
+            print timetran[k][l]
+            print 'Copying settings.csv and params.csv to the transit folder...'
+    
+            if os.path.exists(pathtran):
+                continue
+    
+            cmnd = 'mkdir -p %s' % pathtran
+            print cmnd
+            os.system(cmnd)
+    
+            cmnd = 'cp %ssettings.csv %s' % (pathtmpt, pathtran)
+            print cmnd
+            os.system(cmnd)
+            
+            cmnd = 'cp %sparams.csv %s' % (pathtmpt, pathtran)
+            print cmnd
+            os.system(cmnd)
+            
+            for m, strginst in enumerate(liststrginst):
+                if booltraninst[m]:
+                    path = '%s%s.csv' % (pathtran, strginst)
+                    print 'Writing to %s...' % path
+                    np.savetxt(path, arry[strginst][indx[strginst], :], delimiter=',')
+    
+            for a in range(2):
+                if a == 0:
+                    pathfile = '%ssettings.csv' % pathtran
+                else:
+                    pathfile = '%sparams.csv' % pathtran
+                objtfile = open(pathfile)
+                listline = []
+                
+                for line in objtfile:
+    
+                    if a == 0:
+                        if line.startswith('companions_phot'):
+                            line = 'companions_phot,%s\n' % strgplan
+                            listline.append(line)
+                        elif line.startswith('inst_phot'):
+                            line = 'inst_phot,'
+                            cntr = 0
+                            for m, strginst in enumerate(liststrginst):
+                                if booltraninst[m]:
+                                    if cntr == 0:
+                                        line += strginst
+                                    else:
+                                        line += ' ' + strginst
+                                    cntr += 1
+                            line += '\n'
+                            listline.append(line)
+                        elif 'TESS' in line:
+                            linesplttess = line.split('TESS')
+                            for m, strginst in enumerate(liststrginst):
+                                if booltraninst[m]:
+                                    listline.append(linesplttess[0] + strginst + linesplttess[1])
+                        else:
+                            listline.append(line)
+                            
+                    else:
+                        if line[2:].startswith('epoch'):
+                            strgplantemp = line[0]
+                            linesplt = line.split(',')
+                            if strgplantemp == strgplan:
+                                linesplt[2] = '1'
+                                line = ','.join(linesplt)
+                            listline.append(line)
+                        else:
+                            boolfine = True
+                            for m, strginst in enumerate(liststrginst):
+                                if not booltraninst[m] and strginst in line:
+                                    boolfine = False
+                            if boolfine:
+                                listline.append(line)
+                objtfile.close()
+                print 'Writing to %s...' % pathfile
+                objtfile = open(pathfile, 'w')
+                for line in listline:
+                    #print line
+                    objtfile.write(line)
+                objtfile.close()
+            print
+    
+    print 'indxtranskip'
+    print indxtranskip
+
+    listtimetran = []
+    listtimeresi = []
+    for k, strgplan in enumerate(liststrgplan):
+        for l in indxtran[k]:
+            
+            if l in indxtranskip[k]:
+                continue
+            
+            pathtran = pathdata + 'allesfit_sttv/allesfit_%s%03d/' % (strgplan, l)
+            
+            allesfitter.show_initial_guess(pathtran)
+            allesfitter.mcmc_fit(pathtran)
+            allesfitter.mcmc_output(pathtran)
+            
+            for strgtemp in ['mcmc_fit', 'initial_guess']:
+                cmnd = 'cp %sallesfit_sttv/allesfit_%s%03d/results/%s_%s.pdf %s%s_%s_%d.pdf' \
+                                                    % (pathdata, strgplan, l, strgtemp, strgplan, pathpost, strgtemp, strgplan, l)
+                print cmnd
+                os.system(cmnd)
+            
+            pathsave = pathdata + 'allesfit_sttv/allesfit_%s%03d/results/mcmc_save.h5' % (strgplan, l)
+            print 'Reading %s...' % pathsave
+            emceobjt = emcee.backends.HDFBackend(pathsave, read_only=True)
+            if cntr == 0:
+                numbsamp = emceobjt.get_chain().size
+                timeresi = np.zeros((numbtran[k], numbsamp)) - 1e12
+            timeresi[l, :] = (epoc[k] - emceobjt.get_chain().flatten()) * 24. * 60. # [min]
+        listtimetran.append(timetran[k])
+        listtimeresi.append(timeresi)
+    
+    
+    print 'Making plots...'
+    
+    pathsave = pathdata + 'allesfit_sttv/post/per_transit_ttv.csv'
+    objtfile = open(pathsave, 'w')
+    for a in range(2):
+        figr, axis = plt.subplots(figsize=(12, 6))
+        ylim = 0.
+        for k, strgplan in enumerate(liststrgplan):
+            ydat = np.mean(listtimeresi[k], 1)
+            yerr = np.std(listtimeresi[k], 1)
+            ylim = max(ylim, np.amax(abs(ydat)))
+            axis.errorbar(listtimetran[k], ydat, yerr=yerr, label=strgplan, ls='', marker='o')
+            # write to csv file
+            if a == 0:
+                objtfile.write(strgplan + '\n')
+                for n in range(ydat.size):
+                    objtfile.write('%g, %g\n' % (listtimetran[k][n], ydat[n]))
+        if a == 0:
+            strg = 'zoom'
+            ylim = 10
+        else:
+            strg = 'full'
+            ylim = 50
+        axis.set_xlabel('T [BJD]')
+        axis.set_xlim(limttime)
+        axis.set_ylabel('O-C [min]')
+        axis.set_ylim([-ylim, ylim])
+        axis.axhline(0., ls='--', color='gray', alpha=0.5)
+        axis.legend()
+        path = pathdata + 'allesfit_sttv/post/resi_%s.pdf' % strg
+        print 'Writing to %s...' % path
+        plt.tight_layout()
+        plt.savefig(path)
+        plt.close()
+    objtfile.close()
